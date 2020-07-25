8d6789ee43fcb275242c953d4dfb30cfbad29b11
Nitish Kumar Singh <nitishk72@gmail.com>
2020-07-26 01:32:54 +0530

    initial commit

---
+++ lib/demo.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Demo extends StatefulWidget {
+  @override
+  _DemoState createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  double _result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _heightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'height in cm',
+                icon: Icon(Icons.trending_up),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _weightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'weight in kg',
+                icon: Icon(Icons.line_weight),
+              ),
+            ),
+            SizedBox(height: 15),
+            RaisedButton(
+              color: Colors.pinkAccent,
+              child: Text(
+                "Calculate",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: calculateBMI,
+            ),
+            SizedBox(height: 15),
+            Text(
+              _result == null ? "Enter Value" : "${_result.toStringAsFixed(2)}",
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 19.4,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void calculateBMI() {
+    double height = double.parse(_heightController.text) / 100;
+    double weight = double.parse(_weightController.text);
+
+    double heightSquare = height * height;
+    double result = weight / heightSquare;
+
+    _result = result;
+
+    setState(() {});
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/demo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.pinkAccent,
+      ),
+      home: Demo(),
+    );
+  }
+}
